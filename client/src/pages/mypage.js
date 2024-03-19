156aabdb437ace82d0d336b1308b2c1d45e05e17
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-03-19 13:26:42 +0600

    set category

--- client/src/pages/mypage.js
+++ client/src/pages/mypage.js
@@ -11,8 +11,8 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { styled } from '@mui/material/styles';
 import Badge from '@mui/material/Badge';
 import Avatar from '@mui/material/Avatar';
-import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Accordion, AccordionSummary, AccordionDetails, makeStyles } from '@mui/material';
+
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
     '& .MuiBadge-badge': {
@@ -43,9 +43,22 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
     },
 }));
 
+const StyledAccordion = styled(Accordion)(({ theme }) => ({
+    accordion: {
+        '&.Mui-expanded': {
+            margin: 0,
+        },
+    },
+    accordionSummary: {
+        marginBottom: -1, // Adjust this value to control the expansion direction
+    },
+}));
 
 
 function MyPage({ duas, categories, subCategories }) {
+
+
+
     const [expandedCategoryId, setExpandedCategoryId] = useState(null);
     const [selectedCategoryId, setSelectedCategoryId] = useState(1); // Default category ID
     const [selectedSubCategoryId, setSelectedSubCategoryId] = useState(1); // Default subcategory ID
@@ -66,10 +79,11 @@ function MyPage({ duas, categories, subCategories }) {
     // Check if duas is an array before filtering
     const filteredDuas = Array.isArray(duas) ? duas.filter(dua => dua.cat_id === selectedCategoryId && dua.subcat_id === selectedSubCategoryId) : [];
 
+
     return (
         <>
             <div className="py-10 flex px-12 gap-5">
-                <div className='w-1/12 h-[70vh] rounded-md bg-slate-50 shadow-md mx-auto flex justify-center items-center gap-5 flex-col'>
+                <div className='w-24 h-[70vh] rounded-md bg-slate-50 shadow-md mx-auto flex justify-center items-center gap-5 flex-col'>
                     <div className='w-10 h-10 rounded-full flex justify-center items-center bg-slate-300 shadow-lg'>
                         <OtherHousesIcon style={{ fontSize: 25, color: "gray" }} />
                     </div>
@@ -93,7 +107,7 @@ function MyPage({ duas, categories, subCategories }) {
                     </div>
                 </div>
 
-                <div className="w-11/12  pt-12">
+                <div className="w-full pt-12">
                     <div className='flex justify-between items-center py-5'>
                         <h1 className='text-2xl font-bold'> Dua Page </h1>
                         <div className='flex justify-center items-center w-3/5 relative pb-5'>
@@ -111,49 +125,51 @@ function MyPage({ duas, categories, subCategories }) {
                         >
                             <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
                         </StyledBadge>
-
                     </div>
                     <div className='flex justify-between '>
-                        <div className='w-1/6 h-[70vh] rounded-md bg-slate-50 shadow-md flex justify-center items-center gap-2 flex-col px-2'>
-                            {categories &&
-                                categories.map((category) => (
-                                    <Accordion key={category.cat_id} expanded={expandedCategoryId === category.cat_id} onChange={() => handleAccordionChange(category.cat_id)}>
-                                        <AccordionSummary
-                                            expandIcon={<ExpandMoreIcon />}
-                                            aria-controls={`category-${category.cat_id}-content`}
-                                            id={`category-${category.cat_id}-header`}
-                                        >
-                                            <div key={category.cat_id} onClick={() => handleCategoryClick(category.cat_id)} className='h-14 w-full px-5 bg-white shadow-md rounded-md flex justify-between items-center'>
-                                                <img src={`https://duaruqyah.com/assets/icon/${category?.cat_icon}.svg`} alt={category.cat_icon} />
-                                                <div>
-                                                    <h1 className={`cursor-pointer ${selectedCategoryId === category.cat_id ? 'font-bold' : ''}`}>
-                                                        {category.cat_name_en}
-                                                    </h1>
-                                                    <span> Subcategory:7 </span>
+                        <div className='w-[350px] h-[70vh] bg-slate-50 shadow-md flex justify-center items-center gap-2 flex-col rounded-b-lg'>
+                            <div className=' bg-green-600 w-full h-16 rounded-t-lg flex justify-center items-center'>
+                                <h1 className='text-white font-bold'> Categories </h1>
+                            </div>
+                            <div className="overflow-y-auto h-[60vh]">
+                                {categories &&
+                                    categories.map((category) => (
+                                        <StyledAccordion key={category.cat_id} expanded={expandedCategoryId === category.cat_id} onChange={() => handleAccordionChange(category.cat_id)} >
+                                            <AccordionSummary
+                                                aria-controls={`category-${category.cat_id}-content`}
+                                                id={`category-${category.cat_id}-header`}
+                                            >
+                                                <div key={category.cat_id} onClick={() => handleCategoryClick(category.cat_id)} className='flex justify-between items-center'>
+                                                    <img src={`https://duaruqyah.com/assets/icon/${category?.cat_icon}.svg`} alt={category.cat_icon} />
+                                                    <div>
+                                                        <h1 className={`cursor-pointer ${selectedCategoryId === category.cat_id ? 'font-bold' : ''}`}>
+                                                            {category.cat_name_en}
+                                                        </h1>
+                                                        <span> Subcategory:7 </span>
+                                                    </div>
+                                                    <div>
+                                                    </div>
                                                 </div>
-                                                <div>
-
-                                                </div>
-                                            </div>
-                                        </AccordionSummary>
-                                        <AccordionDetails>
-                                            {subCategories &&
-                                                subCategories.filter(subCat => subCat.cat_id === selectedCategoryId)
-                                                    .map((subCat) => (
-                                                        <div key={subCat.subcat_id} onClick={() => handleSubCategoryClick(subCat.subcat_id)}>
-                                                            <h1 className={`cursor-pointer ${selectedSubCategoryId === subCat.subcat_id ? 'font-bold' : ''}`}>
-                                                                {subCat.subcat_name_bn}
-                                                            </h1>
-                                                        </div>
-                                                    ))}
-                                        </AccordionDetails>
-                                    </Accordion>
-                                ))}
+                                            </AccordionSummary>
+                                            <AccordionDetails>
+                                                {subCategories &&
+                                                    subCategories.filter(subCat => subCat.cat_id === selectedCategoryId)
+                                                        .map((subCat) => (
+                                                            <div key={subCat.subcat_id} onClick={() => handleSubCategoryClick(subCat.subcat_id)}>
+                                                                <h1 className={`cursor-pointer ${selectedSubCategoryId === subCat.subcat_id ? 'font-bold' : ''}`}>
+                                                                    {subCat.subcat_name_en}
+                                                                </h1>
+                                                            </div>
+                                                        ))}
+                                            </AccordionDetails>
+                                        </StyledAccordion>
+                                    ))}
+                            </div>
                         </div>
                         <div className='w-3/5 h-[100vh] rounded-md bg-slate-50 shadow-md'>
                             {filteredDuas.map((dua, index) => (
                                 <div key={index}>
-                                    <h1> {dua.dua_name_bn} </h1>
+                                    <h1> {dua?.dua_name_en} </h1>
                                 </div>
                             ))}
                         </div>
